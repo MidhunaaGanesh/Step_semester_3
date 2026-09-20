f0b5811b71e81c00dfb9e7b3db88d703e7c005a2
MidhunaaGanesh <midhunaaganesh8b@gmail.com>
2026-09-20 16:26:51 +0530

    All Programs Completed

--- Step_sem_3/src/main/java/String_Functions/assignment_programs/WordReversalEncoder.java
+++ Step_sem_3/src/main/java/String_Functions/assignment_programs/WordReversalEncoder.java
@@ -1,4 +1,29 @@
 package String_Functions.assignment_programs;
 
 public class WordReversalEncoder {
+
+    public static String reverseEachWord(String sentence) {
+        if (sentence == null || sentence.trim().isEmpty()) {
+            return "";
+        }
+
+        String[] words = sentence.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            StringBuilder reversedWord = new StringBuilder(words[i]);
+            result.append(reversedWord.reverse());
+
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseEachWord("hello club"));
+    }
 }
+
