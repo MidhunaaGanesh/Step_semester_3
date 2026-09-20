4228bd12367b9c947c31ca8d2f40b94b9fa08495
MidhunaaGanesh <midhunaaganesh8b@gmail.com>
2026-09-20 16:03:52 +0530

    Assignment Program 4 Completed

---
+++ Step_sem_3/src/main/java/OOP/assignment_programs/TableGenerator.java
@@ -0,0 +1,27 @@
+package OOP.assignment_programs;
+
+public class TableGenerator {
+
+    public static void generateFirstValidTable(int[] candidates) {
+        if (candidates == null) {
+            return;
+        }
+
+        for (int num : candidates) {
+            if (num < 1) {
+                System.out.println("Skipping invalid number: " + num);
+                continue;
+            }
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(num + " x " + i + " = " + (num * i));
+            }
+            break;
+        }
+    }
+
+    public static void main(String[] args) {
+        generateFirstValidTable(new int[]{-3, 0, 7, 9});
+    }
+}
+
