270e7a2ea9fcaa159adbc60a259feed66937a877
MidhunaaGanesh <midhunaaganesh8b@gmail.com>
2026-09-20 16:36:41 +0530

    All Programs Completed

--- Step_sem_3/src/main/java/Constructors/class_programs/DuplicateTeamFinder.java
+++ Step_sem_3/src/main/java/Constructors/class_programs/DuplicateTeamFinder.java
@@ -1,4 +1,28 @@
 package Constructors.class_programs;
 
 public class DuplicateTeamFinder {
+
+    static String findDuplicateTeam(String[] teamNames) {
+        if (teamNames == null) {
+            return "No Duplicates Found";
+        }
+
+        for (int i = 0; i < teamNames.length; i++) {
+            for (int j = i + 1; j < teamNames.length; j++) {
+                if (teamNames[i] != null && teamNames[i].equals(teamNames[j])) {
+                    return "Duplicate Found: " + teamNames[i];
+                }
+            }
+        }
+        return "No Duplicates Found";
+    }
+
+    public static void main(String[] args) {
+        String[] teamNames1 = {"ByteForce", "CodeCrafters", "ByteForce"};
+        System.out.println(findDuplicateTeam(teamNames1));
+
+        String[] teamNames2 = {"ByteForce", "CodeCrafters", "NullPointers"};
+        System.out.println(findDuplicateTeam(teamNames2));
+    }
 }
+
