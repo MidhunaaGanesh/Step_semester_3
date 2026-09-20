adb524918b3d8878fa68110915f5813e0003005e
MidhunaaGanesh <midhunaaganesh8b@gmail.com>
2026-09-20 16:45:01 +0530

    All Programs Completed

--- Step_sem_3/src/main/java/ClassesAndObjects/assignment_programs/BookInventory.java
+++ Step_sem_3/src/main/java/ClassesAndObjects/assignment_programs/BookInventory.java
@@ -1,4 +1,33 @@
 package ClassesAndObjects.assignment_programs;
 
-public class BookInventory {
+class BookInventory {
+    String title;
+    String author;
+    int copiesAvailable;
+
+    public BookInventory(String title, String author, int copiesAvailable) {
+        this.title = title;
+        this.author = author;
+        this.copiesAvailable = copiesAvailable;
+    }
+
+    public void printEntry() {
+        System.out.println(title + " by " + author + " - " + copiesAvailable + " copies available");
+    }
 }
+
+public class LibraryInventoryManagement {
+    public static void main(String[] args) {
+        BookInventory[] inventory = {
+                new BookInventory("Clean Code", "Robert C. Martin", 3),
+                new BookInventory("Effective Java", "Joshua Bloch", 5),
+                new BookInventory("Refactoring", "Martin Fowler", 0),
+                new BookInventory("Design Patterns", "GoF", 2)
+        };
+
+        for (BookInventory book : inventory) {
+            book.printEntry();
+        }
+    }
+}
+
